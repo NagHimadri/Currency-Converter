ad8b24db3aa9a81695c1a70f0d39184bb22eaad9
abc <abc@example.com>
2024-03-06 12:49:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const apiKey = "6ed125574e6b3635033fc461";
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for(let select of dropdowns)
+{
+    for(currCode in countryList)
+    {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD")
+        {
+            newOption.selected = "selected";
+        }
+        if(select.name === "to" && currCode === "INR")
+        {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    })
+};
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal <= 0)
+    {
+        alert("Please give a positive rate");
+        amtVal = 1;
+        amount.value = "1";
+    }
+    else if(amtVal >= 1)
+    {
+        let from=fromCurr.value;
+        let to=toCurr.value;
+
+        const URL = `${BASE_URL}/${from}`;
+        let response = await fetch(URL);
+        let data = await response.json();
+        let rate = data.conversion_rates[to];
+
+        let finalAmount = amtVal * rate;
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    }
+    else
+    {
+        alert("Please give a valid input");
+        amtVal = 1;
+        amount.value = "1";
+    }
+};
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
